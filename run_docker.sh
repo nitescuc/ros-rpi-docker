a70fce7b92df25fa2d0af93f1f65362374c66848
Cristian Nitescu <nitescuc@hotmail.com>
2022-12-16 18:56:00 +0000

    enable i2c

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,1 @@
-docker run --restart unless-stopped --name ros -e "ROS_IP=10.42.0.99" -v /home/pi/catkin_ws:/catkin_ws --network host alpine-ros
+docker run --restart unless-stopped --name ros -e "ROS_IP=10.42.0.99" -v /home/pi/catkin_ws:/catkin_ws --network host --device /dev/i2c-1 -u root alpine-ros
